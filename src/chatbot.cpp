c0d3e337af312db89b3cc8489dd28a7f4832f512
Husain Dehnuwala <dehnuwala.husain@gmail.com>
2018-07-10 22:39:17 +0530

    Update chatbot.cpp

--- src/chatbot.cpp
+++ src/chatbot.cpp
@@ -30,24 +30,30 @@ class Bot	{
 				std::getline ( std::cin, userQuery );
 			
 				std::cout << "Bot > ";
-				string category = categorize ( process_input ( userQuery, true ));
-				results =  evaluate_response ( category );
 
-				do	{
-					currResponse = rand () % 3;
-				} while ( prevResponse == currResponse );
-				
-				prevResponse = currResponse;
-				if ( category == "BYE")	execute = false;
+				std::string processedQuery = process_input ( userQuery, true );
+				string category = categorize ( processedQuery );
+				if ( category == "FUNCTION" )	{
+					identify_function ( processedQuery );
+				}
+				else {
+					results =  evaluate_response ( category );
+					do	{
+						currResponse = rand () % 3;
+					} while ( prevResponse == currResponse );
+					
+					prevResponse = currResponse;
+					if ( category == "BYE")	execute = false;
 
-				std::cout << results [ currResponse ];
-				
+					std::cout << results [ currResponse ];
+					
+				}
 			}
 		}
 };
 int main ()		{
 	
 	Bot bot;
-	std::cout << "\033[2J\033[1;1H";
+	system ( "cls || clear ");
 	bot.inititate ();
 }
