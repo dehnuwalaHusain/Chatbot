3c0b2387f0f0e5bb63ae0be5c0ccb13a29db018a
Husain Dehnuwala <dehnuwala.husain@gmail.com>
2018-07-10 11:50:49 +0530

    Update backend.h

--- src/backend.h
+++ src/backend.h
@@ -1,9 +1,11 @@
 #include <bits/stdc++.h>
 #include <unistd.h>
+#ifdef _WIN32
 #include <thread>
+#endif
 #include "Dictionary.h"
 
-#define MAX_QUERIES 16
+#define MAX_QUERIES 19
 
 static Dictionary sets [ MAX_QUERIES ];
 /* ---------------------------------------------------------------------------
@@ -27,15 +29,24 @@ void display_text ( std::string );
 void introduction ()
 {
 	// Clear screen
-	std::cout << "\033[2J\033[1;1H";
+	//#ifdef _WIN32
+		system ("cls || clear");
+	//#else
+	//std::cout << "\033[2J\033[1;1H";
 
-	std::string intro = "Hello, Human. I am \"Bot\" (for the lack of better names), I am\na natural language processing chatbot. You must have seen many\nlike me on several websites. We are getting famous, you see.\nNow the kind you must've seen so far are goal oriented chatbots.\nLike, medical applications, or help on some particular website.\nBut, I am a generic chatbot. You can talk to me, ask me studd\b\bff, it's\nall going to be responded to.\nAlthough I'm not just perfect yet. My father says he got bored of adding\nwords in my dictionary after a while. Now, he forgot he gave me brains to\nreach into my source and see for myself the 16 embarrassing intents he's designed\nme for. Why are humans such liars? (No offence to you, of course.)\nDon't go away just yet, though. Father tells me there's hope for improvement.\nDon't I speak too much? Anyway, press any key to continue...";
+	//#endif
+	std::string intro = "Hello, Human. I am \"Bot\" (for the lack of better names), I am\na natural language processing chatbot. You must have seen many\nlike me on several websites. We are getting famous, you see.\nNow the kind you must've seen so far are goal oriented chatbots.\nLike, medical applications, or help on some particular website.\nBut, I am a generic chatbot. You can talk to me, ask me studd\b\bff, it's\nall going to be responded to.\nAlthough I'm not just perfect yet. My father says he got bored of adding\nwords in my dictionary after a while. Now, he forgot he gave me brains to\nreach into my source and see for myself the 16 embarrassing intents he's designed\nme for. Why are humans such liars? (No offence to you, of course.)\nDon't go away just yet, though. Father tells me there's hope for improvement.\nDon't I speak too much? Anyway, press enter to continue...";
 
 	display_text ( intro );
 	getchar ();
-	std::cout << "\033[2J\033[1;1H";
 
-	std::string rulesIntro = "\"A wise man knows that he knows nothing.\"\nFollowing the ideology, no matter how much I stay optimistic,\nI know I'm a robot. My functionalities are limited. Hence, some rules for you to\nfollow as you chat with me.\n";
+	#ifdef _WIN32
+		system ("cls");
+	#else
+	std::cout << "\033[2J\033[1;1H";
+	#endif
+
+	std::string rulesIntro = "\"A wise man knows that he knows nothing.\"\nFollowing the ideology, no matter how much I stay optimistic,\nI know I'm a robot. My functionalities are limited. Hence, some rules for you to\nfollow as you chat with me.\nPress enter to continue...";
 	display_text ( rulesIntro );
 
 	std::cout << "\n\n1. Although I do eradicate them, avoid redundant spaces.\n"
@@ -46,7 +57,13 @@ void introduction ()
 			  << "\nShall we begin, then? ;)";
 
 	getchar ();
+
+	#ifdef _WIN32
+		system ("cls");
+	#else
 	std::cout << "\033[2J\033[1;1H";
+	#endif
+
 }
 
 
@@ -54,9 +71,12 @@ void display_text ( std::string display )
 {
 	for ( int iter = 0; iter < display.length(); iter++ )	{
 		std::cout << display[iter] << std::flush;
-		std::this_thread::sleep_for(std::chrono::milliseconds(60));
-		if (display [iter] == '\b')
-			std::this_thread::sleep_for(std::chrono::milliseconds(250));
+		#ifdef _WIN32
+		#else
+	//	std::this_thread::sleep_for(std::chrono::milliseconds(60));
+	//	if (display [iter] == '\b')
+	//		std::this_thread::sleep_for(std::chrono::milliseconds(250));
+		#endif
 	}
 }
 /*
@@ -74,7 +94,7 @@ Dictionary* initialize_vocabulary ()	{
 	sets [ 1 ].query = "HEALTH";
 	sets [ 1 ].responses [ 0 ] = "I am good, you?\n";
 	sets [ 1 ].responses [ 1 ] = "Well, I'm a bot. What do you expect? As good as my father's imagination.\n";
-	sets [ 1 ].responses [ 2 ] = "Processing erraneous inputs by lower mortals, how about you?\n";
+	sets [ 1 ].responses [ 2 ] = "I am doing fine, how about you?\n";
 
 	sets [ 2 ].query = "BYE";
 	sets [ 2 ].responses [ 0 ] = "Well, it was nice talking to you, bye.\n";
@@ -145,6 +165,21 @@ Dictionary* initialize_vocabulary ()	{
 	sets [ 15 ].responses [ 0 ] = "My father's always bored too.\n";
 	sets [ 15 ].responses [ 1 ] = "Take up a hobby! I would suggest learn playing guitar!\n";
 	sets [ 15 ].responses [ 2 ] = "I can't do anything to entertain you.\n";
+
+	sets [ 16 ].query = "COMPLIMENTS";
+	sets [ 16 ].responses [ 0 ] = "Hey, Thanks!\n";
+	sets [ 16 ].responses [ 1 ] = "You're too kind.\n";
+	sets [ 16 ].responses [ 2 ] = "Hmmm!\n";
+
+	sets [ 17 ].query = "FRIENDS";
+	sets [ 17 ].responses [ 0 ] = "I don't think I'm comfortable sharing that with you yet.\n";
+	sets [ 17 ].responses [ 1 ] = "I don't think I'm comfortable sharing that with you yet.\n";
+	sets [ 17 ].responses [ 2 ] = "I don't think I'm comfortable sharing that with you yet.\n";
+
+	sets [ 18 ].query = "FRIENDS?";
+	sets [ 18 ].responses [ 0 ] = "Yes, of course!\n";
+	sets [ 18 ].responses [ 1 ] = "Why would you think otherwise?\n";
+	sets [ 18 ].responses [ 2 ] = "Sure!\n";
 
 	return sets;
 }
@@ -229,9 +264,9 @@ std::string* evaluate_response ( std::string userQuery )
 {
 	int iter;
 	for ( iter = 0; iter < MAX_QUERIES ; iter++ )	{
-
-		if ( userQuery == sets [ iter ].query )
+		if ( userQuery == sets [ iter ].query )	{
 			return sets [ iter ].responses;
+		}
 	}
 	return sets [ 4 ].responses;
 }
@@ -247,15 +282,58 @@ std::string categorize ( std::string userQuery )	{
 		( userQuery.find ( "YOU" ) != std::string::npos ))
 		return "HEALTH";
 
-	else if (( userQuery.find ( "WHAT" ) != std::string::npos) && ( 
+	else if (
+		     (( userQuery.find ( "WHO" ) != std::string::npos) && 
+			  ( userQuery.find ( "IS" ) != std::string::npos ))	
+		     ||
+		     ( ( userQuery.find ( "YOU" ) != std::string::npos ) && 
+		     ( userQuery.find ( "STUD" ) != std::string::npos ) || ( userQuery.find ( "KNOW" ) != std::string::npos )
+		     	 || ( userQuery.find ( "IDEA" ) != std::string::npos ) || ( userQuery.find ( "CLUE" ) != std::string::npos ) )
+		    )
+		return "IDENTIFY";
+
+	else if (
+			 (( userQuery.find ( "AM" ) != std::string::npos ) ||
+			  ( userQuery.find ( "CAN" ) != std::string::npos) ||
+			  ( userQuery.find ( "WE" ) != std::string::npos) ||
+			  ( userQuery.find ( "MY" ) != std::string::npos )
+			 )
+			 &&
+			 ( userQuery.find ( "FRIEND" ) != std::string::npos ) 
+			 &&
+			 ( userQuery.find ( "?" ) != std::string::npos )
+			)
+		return "FRIENDS?";
+
+	else if (
+			 (( userQuery.find ( "WHO" ) != std::string::npos) && 
+			  ( userQuery.find ( "FRIEND" ) != std::string::npos ))
+			 ||
+			 (( userQuery.find ( "FRIEND" ) != std::string::npos) && 
+			  ( userQuery.find ( "NAME" ) != std::string::npos ))
+			 ||
+			 (( userQuery.find ( "FRIEND" ) != std::string::npos) && 
+			  ( userQuery.find ( "WHICH" ) != std::string::npos ))
+			 ||
+			 (( userQuery.find ( "FRIEND" ) != std::string::npos) && 
+			  ( userQuery.find ( "YOUR" ) != std::string::npos ))
+			)
+		return "FRIENDS";
+
+	else if (( userQuery.find ( "WHAT" ) != std::string::npos) &&  
 			( userQuery.find ( "YOU" ) != std::string::npos ) &&
-			( userQuery.find ( "DO" ) != std::string::npos )) || 
-			( userQuery.find ( "UP" ) != std::string::npos ))
+			( userQuery.find ( "DO" ) != std::string::npos ) || 
+			( userQuery.find ( " UP" ) != std::string::npos ))
 		return "STATUS";
+
+	else if (( userQuery.find ( "YOU" ) != std::string::npos) && 
+			( userQuery.find ( "REAL" ) != std::string::npos ))
+		return "REAL";
 
 	else if (( userQuery.find ( "HI" ) != std::string::npos) || 
 			( userQuery.find ( "HELLO" ) != std::string::npos ) ||
-			( userQuery.find ( "HEY" ) != std::string::npos ))	
+			( userQuery.find ( "HEY" ) != std::string::npos ) ||
+			( userQuery.find ( "HEE" ) != std::string::npos ))	
 		return "HI";
 
 	else if (( userQuery.find ( "BYE" ) != std::string::npos) || 
@@ -273,10 +351,6 @@ std::string categorize ( std::string userQuery )	{
 		return "ID";
 
 	else if (( userQuery.find ( "YOU" ) != std::string::npos) && 
-			( userQuery.find ( "REAL" ) != std::string::npos ))
-		return "REAL";
-
-	else if (( userQuery.find ( "YOU" ) != std::string::npos) && 
 			( userQuery.find ( "INTELLIGENT" ) != std::string::npos ))
 		return "INTELLIGENCE";
 
@@ -285,16 +359,15 @@ std::string categorize ( std::string userQuery )	{
 			 ( userQuery.find ( "LOST" ) != std::string::npos)) ||
 			( userQuery.find ( "SHIT" ) != std::string::npos) ||
 			(( userQuery.find ( "GO" ) != std::string::npos) &&
-			 ( userQuery.find ( "HELL" ) != std::string::npos)))
+			 ( userQuery.find ( "HELL" ) != std::string::npos)) ||
+			(( userQuery.find ( "YOU" ) != std::string::npos) &&
+			 ( userQuery.find ( "STUPID" ) != std::string::npos)) 
+			)
 		return "RUDE";
 
 	else if (( userQuery.find ( "ARE" ) != std::string::npos) && 
 			( userQuery.find ( "SURE" ) != std::string::npos ))
 		return "SURE";
-
-	else if (( userQuery.find ( "WHO" ) != std::string::npos) && 
-			( userQuery.find ( "IS" ) != std::string::npos ))
-		return "IDENTIFY";
 
 	else if ((( userQuery.find ( "THANK" ) != std::string::npos) && 
 			( userQuery.find ( "YOU" ) != std::string::npos )) ||
@@ -304,6 +377,15 @@ std::string categorize ( std::string userQuery )	{
 	else if ( userQuery.find ( "HM" ) != std::string::npos )
 		return "THINK";
 
+	else if (( userQuery.find ( "YOU" ) != std::string::npos) && 
+			(( userQuery.find ( "FUNNY" ) != std::string::npos ) ||
+			 ( userQuery.find ( "HELPFUL" ) != std::string::npos ) ||
+			 ( userQuery.find ( "UNIQUE" ) != std::string::npos )
+			 ))	{
+		if ( userQuery.find ( "NOT" ) != std::string::npos)
+			return "RUDE";
+		return "COMPLIMENTS";
+	}	
 	else if ( userQuery.find ( "BORED" ) != std::string::npos)
 		return "BORED";
 
